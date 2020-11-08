f4e8ed8b09198b352a9b6c2fb9da22dc064c279c
Muneeb Raza <razamuneeb902@gmail.com>
2020-11-08 15:28:41 +0500

    'done'

--- public/app.js
+++ public/app.js
@@ -1,10 +1,14 @@
+console.log(firebase)
 
-function addNote()
-{
+// create location
+var loc = firebase.database().ref('todos')
 
-    var note = document.getElementById('note')
-    var noteValue = note.value
-    
+// get data from DB
+loc.on('child_added', function(data){
+  console.log(data.val().toDo)
+  var noteValueDB = data.val().toDo  
+
+  
     var tr = document.createElement('tr')
     var td = document.createElement('td')
     
@@ -13,7 +17,7 @@ function addNote()
     
     tr.appendChild(td)
     var codeBullet = String.fromCharCode(7)
-    td.innerHTML = codeBullet + ' ' + noteValue
+    td.innerHTML = codeBullet + ' ' + noteValueDB
     td.setAttribute('class','td')
 
     var td2 = document.createElement('td')
@@ -25,6 +29,7 @@ function addNote()
     var btnEdit = document.createElement('button')
     btnEdit.setAttribute('class','w3-button w3-khaki w3-round-xlarge')
     btnEdit.setAttribute('onClick','editNote(this)')
+    btnEdit.setAttribute('id',data.val().key)
     btnEdit.innerHTML = 'Edit'
 
     // creating delete button
@@ -32,6 +37,7 @@ function addNote()
     var btnDel = document.createElement('button')
     btnDel.setAttribute('class','w3-button w3-amber w3-round-xlarge')
     btnDel.setAttribute('onClick','delNote(this)')
+    btnDel.setAttribute('id',data.val().key)
     btnDel.innerHTML = 'Delete'
 
     var td3 = document.createElement('td')
@@ -39,6 +45,25 @@ function addNote()
     td3.appendChild(btnDel)
     tr.appendChild(td3)
 
+})
+
+function addNote()
+{
+    // targetting value
+    var note = document.getElementById('note')
+    var noteValue = note.value
+
+    // create key
+    var key = loc.push().key
+    console.log(key)
+
+    // create object
+    var todoData = {key:key, toDo:noteValue}
+    
+    // create DB
+    loc.child(key).set(todoData)
+
+    
     note.value = ''
 
 }
@@ -47,7 +72,9 @@ function editNote(e)
 {
     
     var btn = e
-    
+    // console.log(btn.id)
+
+    var keyID = btn.id
     var cell = btn.parentNode
     var row = cell.parentNode
     var cell1 = row.firstChild
@@ -58,7 +85,10 @@ function editNote(e)
     var slicedVal = valToBeEdited.slice(2)
     var slicedIndex = valToBeEdited.slice(0,2)
     var updated = prompt('Update your task',slicedVal)
-    
+
+    // send updated value to DB
+    var updatedObj = {key: keyID, toDo:updated}
+    loc.child(keyID).set(updatedObj)
 
     if(updated === null)
     {
@@ -68,18 +98,17 @@ function editNote(e)
     {
         cell1.innerHTML = slicedIndex + updated
     }
-
-    console.log(btn)
-    console.log(cell)
-    console.log(row)
-    console.log(cell1)
-    console.log(note.value)
 }
 
 // function for deleting individual note
 
 function delNote(d)
 {
+    // get key as id, then remove that object from DB
+    var keyToDel = d.id
+    loc.child(keyToDel).remove()
+
+    // delete from web page
     var rowToBeDel = d.parentNode.parentNode
     var ch0 = rowToBeDel.childNodes[0]
     var ch1 = rowToBeDel.childNodes[1]
@@ -93,6 +122,10 @@ function delNote(d)
 
 function delAll()
 {
+    // remove all from DB
+    loc.remove()
+
+    // remove all from web page
     var delAllNotes = document.getElementById('savedNotes')
     delAllNotes.innerHTML = ''
 }
